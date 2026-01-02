53f806d3328281120548deb4825cf7795e64660d
Guilherme Brandão <guilhermebrandao@MacBook-Pro-de-Guilherme.local>
2026-01-02 19:56:03 -0300

    fix: Remove duplicate code in admin/pedidos/page.tsx

--- app/admin/pedidos/page.tsx
+++ app/admin/pedidos/page.tsx
@@ -20,20 +20,3 @@ export default function AdminOrdersPage() {
 }
 
 
-
-return (
-  <div className="p-4 sm:p-6 space-y-6 h-[calc(100vh-80px)] flex flex-col">
-    <AdminHeader
-      title="Gerenciar Pedidos"
-      description="Gerencie todos os pedidos de delivery em tempo real"
-    />
-
-
-
-    {/* Kanban Board */}
-    <div className="flex-1 min-h-0">
-      <OrdersManager />
-    </div>
-  </div>
-);
-}
