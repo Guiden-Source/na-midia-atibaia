78924886f11e0b1362a422bf3a2d8f4d19d11676
Guilherme Brandão <guilhermebrandao@MacBook-Pro-de-Guilherme.local>
2026-01-06 21:54:43 -0300

    feat: add Google Tag Manager for real-time analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,16 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NFCNPQ3B');`
+        }} />
+        {/* End Google Tag Manager */}
+
         {/* Meta tags específicas para iOS PWA */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
@@ -105,6 +115,16 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           inter.variable
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NFCNPQ3B"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <CartProvider>
           <PWAInstaller />
           <Header />
